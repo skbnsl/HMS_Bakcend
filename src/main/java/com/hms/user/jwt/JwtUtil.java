aeb6f67b1e86af7a32e75c31ebfed1cd0d80a717
skbnsl <bansalsagar13@gmail.com>
2025-01-22 19:20:58 +0530

    test

--- src/main/java/com/hms/user/jwt/JwtUtil.java
+++ src/main/java/com/hms/user/jwt/JwtUtil.java
@@ -16,7 +16,7 @@ public class JwtUtil {
     private static final Long JWT_TOKEN_VALIDITY = 5*60*60L;//in millisecond, for secong = *1000
     private static final String SECRET = "6d089bd7d9a07ef33067c58488e2ab9f0aa59a8b712ae5fb0d23d370aa71fb8a895e8cf423598a707a7107d29a58afaf157ab224108891a5a21d9ffa761f1182";
 
-    public String generateToken(UserDetails userDetails){
+    public String generateToken(UserDetails userDetails) {
         Map<String,Object> claims = new HashMap<>();
         CustomUserDetails user = (CustomUserDetails)userDetails;
     }
