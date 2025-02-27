2eae9c7ca16cac02ccd874530d6c43de186e8c99
skbnsl <bansalsagar13@gmail.com>
2025-02-27 18:43:08 +0530

    some changes

--- src/main/java/com/hms/user/service/UserServiceImpl.java
+++ src/main/java/com/hms/user/service/UserServiceImpl.java
@@ -1,17 +1,19 @@
 package com.hms.user.service;
 
+import java.util.Optional;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.hms.user.dto.LoginDTO;
 import com.hms.user.dto.UserDTO;
 import com.hms.user.entity.User;
 import com.hms.user.exception.HmsException;
 import com.hms.user.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import java.util.Optional;
 
 @Service("UserService")
 @Transactional
@@ -30,7 +32,10 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public void registerUser(UserDTO userDTO) throws HmsException {
+        System.err.print(userDTO);
         logger.info("Registering user: {}", userDTO);
+        String email = userDTO.getEmail().toLowerCase();
+        userDTO.setEmail(email);
         Optional<User> opt = userRepository.findByEmail(userDTO.getEmail());
         if(opt.isPresent()){
             throw new HmsException("USER_ALREADY_EXIST");
@@ -45,7 +50,7 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public UserDTO loginUser(LoginDTO userDTO) throws HmsException {
-        User user= userRepository.findByEmail(userDTO.getEmail()).orElseThrow(()->new HmsException("USER_NOT_FOUND"));
+        User user= userRepository.findByEmail(userDTO.getEmail().toLowerCase()).orElseThrow(()->new HmsException("USER_NOT_FOUND"));
         if(!passwordEncoder.matches(userDTO.getPassword(), user.getPassword())){
             throw new HmsException("INVALID_CREDENTIALS");
         }
